7eb3e42b7b00b744ac146da320aef56704b7f799
Fridolin Pokorny <fridolin@redhat.com>
2018-03-14 15:54:28 +0100

    Project import

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+os.chdir('graphexp/')
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
